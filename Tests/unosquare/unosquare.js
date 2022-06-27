8a6485fa4026cf807b43044c88cb6618ae2ee567
LilianZuritaCardozo <lilian.zurita@hotmail.com>
2022-06-27 16:37:15 -0400

    adding scripts with command page objects

---
+++ Tests/unosquare/unosquare.js
@@ -0,0 +1,47 @@
+
+
+//module.exports = {
+  //'test pages validation' : function(browser) {
+    //var unosquare = browser.page.mainpage();
+
+   // unosquare
+   // .navigate()
+   // .waitForElementVisible('@contactusMenu')
+   // .click('@contactusMenu')
+   // .setValue('@contactField', 'testName')
+   // .setValue('@emailField', 'ana@gmail.com')
+   // .click('@submitButton')
+   // .assert.textContains('@thankyouMessage', 'THANK YOU!')
+   // .assert.textContains('@thankyouMessage', 'FOR FILLING OUR FORM, WE WILL BE SURE TO CONTACT YOU SHORTLY')
+  //  browser.end();
+ // },
+
+  //'test pages validation 1' : function(browser) {
+  // var unosquare = browser.page.mainpage();
+
+  //unosquare
+ // .navigate()
+ // .waitForElementVisible('@blogButton')
+ // .click('@blogButton')
+ // .assert.title('Software Development Blog | Digital Transformation Blog')
+ // .assert.cssProperty('@blogLogo', 'padding-top', '5px')
+ // .verify.not.cssProperty('@blogLogo', 'padding-top', '5px')
+ // browser.end();
+  //}, 
+
+ // 'test pages validation 2' : function(browser) {
+ //  var unosquare = browser.page.mainpage();
+
+  //  unosquare
+   // .navigate()
+   // .waitForElementVisible('@aboutMenu')
+   // .click('@aboutMenu')
+   // .assert.attributeContains('@aboutStoreButton', 'href', 'https://store.unosquare.com/')
+  //  .click('@aboutStoreButton')
+   // .assert.urlEquals('https://store.unosquare.com/')
+  //  browser.end();
+ // }
+//};
+
+
+
